9a093057440578d3aab4f651c00a9aebd1602451
Norman <nkang2020@gmail.com>
2017-12-18 10:32:42 -0800

    Version-2.6.2

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 require('./client/zingchart.min.js');
-zingchart.MODULESDIR = "https://cdn.zingchart.com/2.6.1/modules/";
+zingchart.MODULESDIR = "https://cdn.zingchart.com/2.6.2/modules/";
 module.exports = zingchart;

b97e77686f0816677191d6dae27602f746d3f5f2
Norman Kang <normankang@Normans-MacBook-Pro.local>
2018-10-03 10:37:45 -0700

    Version-2.8.2

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 require('./client/zingchart.min.js');
-zingchart.MODULESDIR = "https://cdn.zingchart.com/2.8.1/modules/";
+zingchart.MODULESDIR = "https://cdn.zingchart.com/2.8.2/modules/";
 module.exports = zingchart;

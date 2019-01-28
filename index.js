1930dcb27d6a6734b1c236d27f811598c012f840
Norman Kang <normankang@Normans-MacBook-Pro.local>
2019-01-28 10:51:09 -0800

    Version-2.8.5

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 require('./client/zingchart.min.js');
-zingchart.MODULESDIR = "https://cdn.zingchart.com/2.8.4/modules/";
+zingchart.MODULESDIR = "https://cdn.zingchart.com/2.8.5/modules/";
 module.exports = zingchart;
